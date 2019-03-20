32d3c8c4dc4b7a3b62cf40dacc0f3b33b5925784
Adam Clark <adam.m.clark@accenture.com>
2019-03-20 12:03:45 -0600

    Updating to Nest 6.0
    This updates the dependencies, it's implementation in index.ts and also the e2e tests.
    
    serverless-offline was added to create a more complete e2e test.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,33 @@
-import request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import * as r from 'request';
+import { spawn, ChildProcess } from 'child_process';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('Serverless Offline (e2e)', () => {
+  let server: ChildProcess;
+  let testUrl: String;
+  beforeAll((done) => {
+    server = spawn('./node_modules/.bin/serverless', ['offline']);
+    server.stdout.on('data', function (data) {
+      const match = data.toString().match(/Offline listening on (.*)/);
+      if (match) {
+        testUrl = match[1];
+        done();
+      }
+    });
 
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    server.stderr.on('data', function (data) {
+      console.error('stderr:' + data);
+    });
   });
 
-  it('/GET /', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    server.kill('SIGINT')
+  });
+
+  it('should return status code 200', (done) => {
+    r(testUrl, (err, response, body) => {
+      expect(err).toEqual(null);
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
   });
 });
